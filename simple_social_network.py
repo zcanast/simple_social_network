eea900893ba43328a229136da94a7a38c2a4841d
Zoey Anastasiadis <zcanast@gmail.com>
2021-10-27 10:24:39 -0400

    added simple_social_network.py

---
+++ simple_social_network.py
@@ -0,0 +1,11 @@
+""" A simple social network for tracking networks between people"""
+
+class Person:
+    """ Represents a person in a social network
+    
+    Attrinutes:
+        name (str): the person's name
+        connections (set of Person): other people in the social network who know
+            this person.
+    
+    """
